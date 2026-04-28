7fedb50bf7e0eb59725efc3dd2cbb356c6c59f42
purnimapant77 <purnimaaaa7@gmail.com>
2026-04-28 19:10:45 +0545

    template engine

--- mycode/mycode/urls.py
+++ mycode/mycode/urls.py
@@ -18,9 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from coding.views import *
+from scorecard.views import *
 
 urlpatterns = [
     path("",home, name="home"),
+    path("players/", players, name="players"),
     path("success_page/",success_page, name="success_page"),
     path("admin/", admin.site.urls),
 ]
---
+++ mycode/scorecard/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def players(request):
+    peoples=[
+        {'name':"Purnima Pant", 'age':20},
+        {'name':"Nidhi Pant", 'age':12},
+        {'name':"Ananya Bhatta", 'age':10},
+        {'name':"Anni Pathak", 'age':22}
+    ]
+    return render(request, 'home/index.html', context={"peoples":peoples})
