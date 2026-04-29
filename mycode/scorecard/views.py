3b72585e4d755432548c99e36bdd629419b4bf80
purnimapant77 <purnimaaaa7@gmail.com>
2026-04-29 08:27:47 +0545

    loops and conditions

--- mycode/scorecard/views.py
+++ mycode/scorecard/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 
 def players(request):
     peoples=[
-        {'name':"Purnima Pant", 'age':20},
-        {'name':"Nidhi Pant", 'age':12},
-        {'name':"Ananya Bhatta", 'age':10},
-        {'name':"Anni Pathak", 'age':22}
+        {'name':"Purnima Pant", 'age':20, 'score':100},
+        {'name':"Nidhi Pant", 'age':12, 'score':250},
+        {'name':"Ananya Bhatta", 'age':10, 'score': 90},
+        {'name':"Anni Pathak", 'age':22, 'score':120}
     ]
     return render(request, 'home/index.html', context={"peoples":peoples}) #when we need to send data from bakhend to template we use context
