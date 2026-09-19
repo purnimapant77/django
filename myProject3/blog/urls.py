8cc2eae9bfa0fc757befb88e5e774081b6d5903c
purnimapant77 <purnimaaaa7@gmail.com>
2026-09-19 09:14:26 +0545

    kwargs url parameter

--- myProject3/blog/urls.py
+++ myProject3/blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns=[
     path('post_detail/<int:post_id>/',views.post_detail, name='post-detail'),
     path('user_detail/<str:username>/',views.user_detail, name='user-detail'),
-    re_path(r'^article_by_years/(?P<year>[0-9]{4})/$',views.article_by_years, name='article-by-year'),
+    path('article_date/<int:year>/<int:month>/<int:day>/',views.article_date, name='article-date'), #multiple parameters using path
+    re_path(r'^article_by_years/(?P<year>[0-9]{4})/$',views.article_by_years, name='article-by-year'), #repath
+    #mulriple argumants using kwargs
+    path('birthday/<int:year>/<int:month>/',views.Birthday, name='birthday'), #multiple parameters using path
 ]
--- myProject3/blog/views.py
+++ myProject3/blog/views.py
@@ -9,5 +9,11 @@ def post_detail(request,post_id):
 def user_detail(request, username):
     return HttpResponse(f"<h3>your username is {username}</h3>")
 
+def article_date(request, year, month, day):
+    return HttpResponse(f"<h3>your date is {year}-{month}-{day}</h3>")
+
 def article_by_years(request, year):
     return HttpResponse(f"<h3>Articles from {year}</h3>")
+
+def Birthday(request, **kwargs):
+    return HttpResponse(f"<h3>your birth date is {kwargs} </h3>")
